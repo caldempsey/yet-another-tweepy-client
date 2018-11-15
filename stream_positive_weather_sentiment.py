6e1d5d0cb82a3a32ab2c96300acfe2f3e29723d5
mmacheerpuppy <callum.leach@hotmail.co.uk>
2018-11-15 11:18:21 +0000

    Initial build.

--- stream_positive_weather_sentiment.py
+++ stream_positive_weather_sentiment.py
@@ -1,12 +1,48 @@
 # Example script on how to use the suite of Tweepy compliant to build scripts
 # Particularly helpful for NiFi when we don't want any extra STDOut.
+import json
 import sys
 
-from tweepy import Stream, OAuthHandler
+from tweepy import Stream, OAuthHandler, StreamListener
 
 from yatc import settings
 from yatc.consumers import STDConsumer
 from yatc.settings import validate_json_config
+
+
+# Override to extract only meaningful metadata
+class SentimentConsumer(StreamListener):
+    """
+    A TweetConsumer is responsible for listening to a Tweepy stream and ingesting its data.
+    The Base Consumer is responsible only for returning data (tweets) ingested and can be hooked onto.
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Initialises the consumer, inherits from Tweepy Stream Listener. Achieves nothing more than returns data directly
+        to processes hooked onto the function.
+        """
+        super().__init__()
+
+    def on_data(self, data):
+        """
+        Override from the Tweepy on_data documentation.
+        """
+        # Load JSON into keys to extract values
+        data = json.loads(data)
+        # Dump data output with expected vars.
+        out = {"id": data["id"], "text": data["text"], "positive_sentiment": True}
+        print(json.dumps(out))
+
+    def on_error(self, status):
+        """
+        Override from the Tweepy on_error documentation.
+        """
+        if status == 420:
+            # Returning false will disconnect the stream.
+            return False
+        return True
+
 
 # Check to see if the secrets stored in configuration variables are valid otherwise terminate the script.
 if not validate_json_config:
